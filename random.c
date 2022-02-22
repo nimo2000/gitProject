e6be7909a2b9d6cd9834e78eb03b28ee7d71c992
nimo2000 <ahmed.khalil@egx.com.eg>
2022-02-22 11:28:26 +0200

    my second commit to random.c file

--- random.c
+++ random.c
@@ -20,3 +20,30 @@ int main(int argc, char *argv[])
 
   return EXIT_SUCCESS;
 }
+
+=======================
+this my second  version of the code
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int random_int(int max)
+{
+  return rand() % max;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  int max = atoi(argv[1]);
+
+  srand(time(NULL));
+  int result = random_int(max);
+  printf("%d\n", result);
+
+  return EXIT_SUCCESS;
+}
